aae3f1bf1477d48cbb4f751cc94d8c229f72c2fd
Adrienne <87454125+AdrienneCallie13@users.noreply.github.com>
2021-09-10 18:04:05 +1000

    Add files via upload

---
+++ Task 3/app.vue
@@ -0,0 +1,68 @@
+<template>
+  <form :class="this.errors ? 'errors' : false">
+    <div>
+      <label for="name">Name:</label>
+      <input id="name" v-model="name" required @invalid="invalidateForm" />
+      <span class="error-message">Required</span>
+    </div>
+    <div>
+      <label for="email">Email:</label>
+      <input
+        id="email"
+        type="email"
+        v-model="email"
+        required
+        @invalid="invalidateForm"
+      />
+      <span class="error-message">Required</span>
+    </div>
+    <div>
+      <label for="text">Comment:</label>
+      <textarea id="text" v-model="text"></textarea>
+    </div>
+    <div>
+      <input type="submit" />
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      errors: false
+    };
+  },
+  methods: {
+    invalidateForm() {
+      this.errors = true;
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  padding: 2em;
+}
+label {
+  display: block;
+}
+div + div {
+  margin-top: 1em;
+}
+.error-message {
+  display: none;
+  color: red;
+}
+
+/* ✨ The magic ✨ */
+form.errors {
+  :invalid {
+    border-color: red;
+  }
+  .error-message {
+    display: block;
+  }
+}
+</style>
