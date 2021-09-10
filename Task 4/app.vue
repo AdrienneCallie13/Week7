16acb5ef81f22e179729cbcdf673f4ba98a8f1b1
Adrienne <87454125+AdrienneCallie13@users.noreply.github.com>
2021-09-10 18:06:26 +1000

    Add files via upload

---
+++ Task 4/app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Post>
+      <template v-slot:name>
+         <h1>What is Lorem Ipsum?</h1>
+      </template>
+      <template v-slot:description>
+        <p>
+          What is Lorem Ipsum?Lorem Ipsum is simply dummy text
+       </p>
+     </template>
+    </Post>
+  </div>
+</template>
+
+<script>
+import Post from "./components/Post";
+export default {
+  components: {
+    Post
+  }
+};
+</script>
